9ee50a3a041b791514d013c98c1e3b47374f525c
Besfort21 <besfort-avdyli1@hotmail.com>
2023-02-23 19:50:00 +0100

    HTML setup and Grid setup for the game

---
+++ script.js
@@ -0,0 +1,11 @@
+let field = 16*16;
+
+const parent = document.getElementById('container');
+
+
+for(let i = 0;i<field;i++){
+    let div = document.createElement('div');
+    div.classList.add('square');
+    parent.appendChild(div);
+    div.innerHTML = "HEY"
+}
